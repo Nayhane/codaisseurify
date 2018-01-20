5a4b5ef3e7fa9b89f446043c4229a7994112a305
NAYHANE <NAYHANENE@HOTMAIL.com>
2018-01-20 18:28:18 +0100

    Controller and view Artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Artist.destroy_all
+
+# Artist
+adele = Artist.create!(name: "Adele")
+ed_sheeran = Artist.create!(name: "Ed Sheeran")
+coldplay = Artist.create!(name: "Coldplay")
+u_2 = Artist.create!(name: "U2")
