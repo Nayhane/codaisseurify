714af516809014517012ec0b59624edb184f1158
NAYHANE <NAYHANENE@HOTMAIL.com>
2018-01-28 12:53:26 +0100

    delete in progress

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -5,77 +5,83 @@ function submitSong(event) {
   $("#song_name").val(null);
 }
 
-
 function createSong(name, artist_id){
-    let newSong = {name: name}
 
+  let newSong = {name: name};
 
   $.ajax({
     type: "POST",
     url: "/api/artists/" + artist_id + "/songs.json",
     data: JSON.stringify({
-        song: newSong
+      song: newSong
     }),
     contentType: "application/json",
     dataType: "json"
   })
-  .done(function(data) {
-    console.log(data);
 
+.done(function(data){
+  console.log(data);
 
-    let songId = data.id;
+  let songId = data.id;
 
-    let button = $('#button').on('click', '#btn_a', function(){
-     console.log(this.value);
+  let button = $('#button').on('click', '#btn_a', function(){
+    console.log(this.value);
  });
 
-
-    let listItem = $('<li class="song_list"></li>')
-    .attr('id', songId)
-    .html(name);
-
-
-    $("#song_list").append(listItem).append(button);
+  let listItem = $('<li class="song_list"></li>')
+  .attr('id', songId)
+  .html(name)
 
 
-  })
+  $("#song_list").append(listItem).append(button);
 
-  .fail(function(error) {
-  console.log(error)
-  error_message = error.responseJSON.name[0];
-  showError(error_message);
-});
 
+})
+.fail(function(error) {
+    console.log(error)
+    error_message = error.responseJSON.title[0];
+    showError(error_message);
+  });
 }
 
 
-// function destroySong(event) {
-//   event.preventDefault();
-//   $.when($("click").remove())
-// // }
-// function deleteSong(event) {
-//   event.preventDefault();
-//
-//   let deleteItems = document.getElementById('delete');
-// }
-//
-// function destroySong(songId){
-//   $.ajax({
-//     type: "DELETE",
-//     url: "/api/artists/" + artist_id + "/songs.json",
-//     contentType: "application/json",
-//     dataType: "json"
-//   })
-//
-//   .done(function(data) {
-//     $('tr[data-id="' + songId +'"]').remove();
-//   });
-// }
+function destroySong(event){
+  event.preventDefault();
+  deleteSong($("#button"));
+
+
+.done(function(data){
+    console.log(data);
+
+let songId = data.id;
+
+let deleteBtn = $('<button></button>')
+.attr('id', songId)
+.val('id_song')
+.bind('click', deleteSong)
+.html('Delete')
+
+
+function(index, deleteBtn){
+  SongId = $(listItem).data('id');
+  deleteSong(songId);
+});
+
+ $.ajax({
+   type: "DELETE",
+   url: "/api/artists/" + artist_id + "/songs/" + song_id + ".json",
+   data: JSON.stringify({
+     song: newSong
+   }),
+   contentType: "application/json",
+   dataType: "json"
+
+ })
+}
 
 
 
 $(document).ready(function() {
-    $("form").bind('submit', submitSong);
-    $("#delete").bind('click', destroySong);
-
+  $("form").bind('submit', submitSong);
+  $("#button").bind('onclick', destroySong);
 });
